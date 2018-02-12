a1910d1f24a21a61ef01ade9de710fbbd36f662f
Tobias Andersson <toan01@betssongroup.com>
2018-02-12 15:41:39 +0100

    Started on login screen

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,9 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import {AccordionModule} from 'primeng/accordion';     //accordion and accordion tab
+import {MenuItem} from 'primeng/api';    
+import {InputTextModule} from 'primeng/inputtext';            
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: [
+    './login.component.scss'
+  ]
 })
 export class LoginComponent implements OnInit {
 
