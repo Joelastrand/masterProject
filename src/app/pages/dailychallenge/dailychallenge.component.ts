e5d5213b366ed55a449abaaae13b5ea90baf8157
Tobias Andersson <toan01@betssongroup.com>
2018-03-06 15:23:02 +0100

    Daily challenge randoms a challenge from the firebase dailyChallenge list

--- src/app/pages/dailychallenge/dailychallenge.component.ts
+++ src/app/pages/dailychallenge/dailychallenge.component.ts
@@ -3,8 +3,9 @@ import { NgCircleProgressModule } from 'ng-circle-progress';
 import { SimpleTimer } from 'ng2-simple-timer';
 import { OnChanges } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AngularFireDatabase } from 'angularfire2/database';
+import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
 import { Observable } from 'rxjs/Observable';
+import "rxjs/add/operator/map";
 
 @Component({
   selector: 'app-dailychallenge',
@@ -14,71 +15,92 @@ import { Observable } from 'rxjs/Observable';
 
 
 
+
 export class DailychallengeComponent implements OnInit {
+  //Timer variables
   timerValue = 0.00;
-  maxTimerValue = 1800;
   counter: number = 0;
   timerId: string;
-  timerVisibility = "hidden";
-  randomChallengeName: string;
-  listOfChallenges: [{"hej"}];
+  timer = false;
+  time = 0;
+
+  //Challenge variables
+  challengeName = "";
+  challengeParameters = {};
+  
+
   constructor(private st: SimpleTimer, private db: AngularFireDatabase) { }
 
-  getRandomInt(max) {
-    return Math.floor(Math.random() * Math.floor(max));
+  ngOnInit() {
+    
+
   }
 
-  pickRandomProperty() {  
+  fetchRandomChallenge() {  
     var list = [];
     var query = this.db.database.ref("challenges/dailyChallenges").orderByKey();
+    var count = 0;
+    var randomNumber = this.getRandomInt(3);
+
+    var setName = (name) => {this.setChallengeName(name)};
+    var setChallengeParams = (para) => {this.setChallengeParameter(para)};
+
     query.once("value")
     .then(function(snapshot) {    
       snapshot.forEach(function(childSnapshot) {
         var key = childSnapshot.key;
         var childData = childSnapshot.val();
-        var challengeObject = {key, childData};
-        list.push(challengeObject);
-      
+        if(randomNumber == count) {
+          setName(key);
+          setChallengeParams(childData);
+        }
+        count ++;    
       });
     });
-    return list;
   }
 
-  
-  ngOnInit() {
-    var challengeList = [];
-    var randomNumber = this.getRandomInt(3);
-    challengeList = this.pickRandomProperty();
-    console.log(challengeList);
-    console.log(challengeList[0]);
-    console.log(challengeList[randomNumber]);
-    console.log(randomNumber);
-    //this.challengeList = this.getLeaderboard('/challenges/dailyChallenges/');
-   // console.log(this.pickRandomProperty(this.challengeList));
- 
-  }
-
-
-  incrementTime() {
-    this.counter++;
-    this.timerValue = (this.counter / this.maxTimerValue) * 100;
-    if (this.counter >= this.maxTimerValue) {
-      this.st.unsubscribe(this.timerId);
-      this.st.delTimer('secondCounter');
-    }
-  }
 
   getRandomChallenge() {
-    this.timerVisibility = this.timerVisibility == "hidden" ? "shown" : "hidden";
+    this.fetchRandomChallenge();
   }
 
   initiateChallenge() {
-    this.st.newTimer('secondCounter', 0.001);
+    this.st.newTimer('secondCounter', 0.0001);
     this.timerId = this.st.subscribe('secondCounter', () => this.incrementTime());
   }
+  
 
   finishChallenge() {
+    console.log("Complete!");
+  }
 
+
+  
+
+  incrementTime() {
+    this.counter++;
+    var maxTimerValue = this.time*60;
+    this.timerValue = (this.counter / maxTimerValue) * 100;
+    if (this.counter >= maxTimerValue) {
+      this.st.unsubscribe(this.timerId);
+      this.st.delTimer('secondCounter');
+      this.finishChallenge();
+    }
+  }
+
+  
+  setChallengeName(name: any = null): any{
+    this.challengeName = String(name);
+  }
+  setChallengeParameter(param: any = null): any{
+    this.challengeParameters = param;
+    if(this.challengeParameters["Timer"]) {
+      this.timer = this.challengeParameters["Timer"];
+      this.time = this.challengeParameters["Time"];
+    }
+  }
+  getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
   }
 
 }
