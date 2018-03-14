c8a3e41432c43d2befc05a3c6e5e0eaa42e2c50f
Tobias Andersson <toan01@betssongroup.com>
2018-03-14 15:38:14 +0100

    Continued on sequence on dailychallenge

--- src/app/pages/dailychallenge/dailychallenge.component.ts
+++ src/app/pages/dailychallenge/dailychallenge.component.ts
@@ -18,45 +18,45 @@ import "rxjs/add/operator/map";
   animations: [
     trigger('timer', [
       state('true', style({
-          opacity: 1,
-          display: "flex"
+        opacity: 1,
+        display: "flex"
       })),
       state('false', style({
-          opacity: 0,
-          display: "none"
+        opacity: 0,
+        display: "none"
       })),
       transition('false => true', animate('1000ms ease-in')),
     ]),
     trigger('sequence', [
       state('true', style({
-          opacity: 1,
-          display: "flex"
+        opacity: 1,
+        display: "flex"
       })),
       state('false', style({
-          opacity: 0,
-          display: "none"
+        opacity: 0,
+        display: "none"
       })),
       transition('false => true', animate('1000ms ease-in')),
     ]),
     trigger('getChallengeButton', [
       state('1', style({
-          opacity: 1,
-          display: "flex"
+        opacity: 1,
+        display: "flex"
       })),
       state('0', style({
-          opacity: 0,
-          display: "none"
+        opacity: 0,
+        display: "none"
       })),
       transition('1 => 0', animate('2ms ease-out')),
     ]),
     trigger('challengeInfo', [
       state('0', style({
-          opacity: 1,
-          display: "flex"
+        opacity: 1,
+        display: "flex"
       })),
       state('1', style({
-          opacity: 0,
-          display: "none"
+        opacity: 0,
+        display: "none"
       })),
       transition('1 => 0', animate('1000ms ease-in')),
     ])
@@ -71,9 +71,11 @@ export class DailychallengeComponent implements OnInit {
   showGetChallengeButton = 1;
   challengeFinished = false;
 
+
   //Sequence variables
   sequence = false;
   sequenceList = [];
+  sequenceStarted = false;
 
   //Timer variables
   timerValue = 0.00;
@@ -89,46 +91,46 @@ export class DailychallengeComponent implements OnInit {
   username: string;
 
   constructor(private afAuth: AngularFireAuth, private auth: AuthService, private st: SimpleTimer, private db: AngularFireDatabase) { }
-  
+
   async getUser() {
-	  const res = await this.auth.getUserName().subscribe(uname => {
-			this.username = uname.$value;
-    });
-    
+    /*const res = await this.auth.getUserName().subscribe(uname => {
+      this.username = uname.$value;
+    });*/
+
   }
-  
+
   ngOnInit() {
     //this.getUser();
   }
 
-  fetchRandomChallenge() {  
+  fetchRandomChallenge() {
     var list = [];
     var query = this.db.database.ref("challenges/dailyChallenges").orderByKey();
     var count = 0;
     var randomNumber = this.getRandomInt(2); //TODO: Change to the number of challenges in database, sry for hardcode
 
-    var setName = (name) => {this.setChallengeName(name)};
-    var setChallengeParams = (para) => {this.setChallengeParameter(para)};
+    var setName = (name) => { this.setChallengeName(name) };
+    var setChallengeParams = (para) => { this.setChallengeParameter(para) };
 
     query.once("value")
-    .then(function(snapshot) {    
-      snapshot.forEach(function(childSnapshot) {
-        var key = childSnapshot.key;
-        var childData = childSnapshot.val();
-        if(randomNumber == count/*String(key)=="ExerciseSequence"*/) {
-          setName(key);
-          setChallengeParams(childData);
-        }
-        count ++;    
+      .then(function (snapshot) {
+        snapshot.forEach(function (childSnapshot) {
+          var key = childSnapshot.key;
+          var childData = childSnapshot.val();
+          if (/*randomNumber == count*/String(key)=="ExerciseSequence") {
+            setName(key);
+            setChallengeParams(childData);
+          }
+          count++;
+        });
       });
-    });
   }
 
   //TODO: Add functionality for more types of challenges, currently only works for timer challenges...
   getRandomChallenge() {
-    
-    var d = new Date(); 
-    var date=""+d.getFullYear()+"-"+(d.getMonth()+1)+"-"+(d.getDate());
+
+    var d = new Date();
+    var date = "" + d.getFullYear() + "-" + (d.getMonth() + 1) + "-" + (d.getDate());
 
     var challengeNotDoneToday = () => {
       this.fetchRandomChallenge();
@@ -140,28 +142,32 @@ export class DailychallengeComponent implements OnInit {
 
     //Check if daily challenge has been made today
     this.db.database.ref("scores/" + this.username + "/dailyChallenge/date").once("value")
-    .then(function(snapshot) { 
-      console.log(new Date(snapshot.val()));
-      console.log(new Date(date));
-      if(new Date(snapshot.val()).getTime() != new Date(date).getTime()+1) {
-        challengeNotDoneToday();
-      } else {
-        challengeDoneToday();
-      }
-    });
+      .then(function (snapshot) {
+        console.log(new Date(snapshot.val()));
+        console.log(new Date(date));
+        if (new Date(snapshot.val()).getTime() != new Date(date).getTime() + 1) {
+          challengeNotDoneToday();
+        } else {
+          challengeDoneToday();
+        }
+      });
 
   }
 
-  initiateChallenge() { //Called when "start-button" pressed, should not start timer for all
-  
-  if(this.timer) {
-    this.timerOn = true;
-    this.st.newTimer('secondCounter', 0.00001);
-    this.timerId = this.st.subscribe('secondCounter', () => this.incrementTime());
+  initiateSequence() {
+    this.sequenceStarted = true;
+    console.log(this.sequenceList[0]);
   }
-    
+  initiateChallenge() { //Called when "play-button" pressed
+
+    if (this.timer) {
+      this.timerOn = true;
+      this.st.newTimer('secondCounter', 0.00001);
+      this.timerId = this.st.subscribe('secondCounter', () => this.incrementTime());
+    }
+
   }
-  
+
 
   finishChallenge() {
 
@@ -174,14 +180,14 @@ export class DailychallengeComponent implements OnInit {
 
   updateDailyChallenge() {
     //Increments streak and updates "date" to current date
-    var d = new Date(); 
-    var date=""+d.getFullYear()+"-"+(d.getMonth()+1)+"-"+(d.getDate());
-    var updateStreak = (newStreak) => {this.db.object(`/scores/${this.username}/dailyChallenge`).update({"streak": newStreak})};
+    var d = new Date();
+    var date = "" + d.getFullYear() + "-" + (d.getMonth() + 1) + "-" + (d.getDate());
+    var updateStreak = (newStreak) => { this.db.object(`/scores/${this.username}/dailyChallenge`).update({ "streak": newStreak }) };
     this.db.database.ref("scores/" + this.username + "/dailyChallenge/streak").once("value")
-    .then(function(snapshot) { 
-      updateStreak(snapshot.val()+1);   
-    });
-    this.db.object(`/scores/${this.username}/dailyChallenge`).update({"date": date});
+      .then(function (snapshot) {
+        updateStreak(snapshot.val() + 1);
+      });
+    this.db.object(`/scores/${this.username}/dailyChallenge`).update({ "date": date });
   }
 
 
@@ -193,7 +199,7 @@ export class DailychallengeComponent implements OnInit {
 
   incrementTime() {
     this.counter++;
-    var maxTimerValue = this.time*60;
+    var maxTimerValue = this.time * 60;
     this.timerValue = (this.counter / maxTimerValue) * 100;
     if (this.counter >= maxTimerValue) {
       this.st.unsubscribe(this.timerId);
@@ -201,25 +207,25 @@ export class DailychallengeComponent implements OnInit {
       this.finishChallenge();
     }
   }
-  
-  setChallengeName(name: any = null): any{
+
+  setChallengeName(name: any = null): any {
     this.challengeName = String(name);
   }
 
 
-  setChallengeParameter(param: any = null): any{
+  setChallengeParameter(param: any = null): any {
     this.challengeParameters = param;
     //TODO: Continue this when adding more types of challenges
-    if(this.challengeParameters["Timer"]) { //Parameters if timer
+    if (this.challengeParameters["Timer"]) { //Parameters if timer
       this.timer = this.challengeParameters["Timer"];
       this.time = this.challengeParameters["Time"];
-    } else if(this.challengeParameters["Sequence"]) { //Parameter if sequence
+    } else if (this.challengeParameters["Sequence"]) { //Parameter if sequence
       this.sequence = this.challengeParameters["Sequence"];
       for (var key in this.challengeParameters["Exercises"]) {
-        var exerciseObject = {name: key, repeats: this.challengeParameters["Exercises"][key]};
+        var exerciseObject = { name: key, repeats: this.challengeParameters["Exercises"][key] };
         this.sequenceList.push(exerciseObject);
         console.log(exerciseObject);
-        
+
       }
 
     }
